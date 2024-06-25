87d853ea8dd8bfd75caafdf6a4fd5c21594e301e
MacMelendez <102198069+MacMelendez@users.noreply.github.com>
2024-06-25 15:40:38 -0400

    Display Attempt

--- .vscode/demo/src/main/java/com/programmingeasy/demo/ProgrammingEasy.java
+++ .vscode/demo/src/main/java/com/programmingeasy/demo/ProgrammingEasy.java
@@ -2,18 +2,22 @@ package com.programmingeasy.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@Controller
 @SpringBootApplication
+
 
 public class ProgrammingEasy {
     
     
     @RequestMapping("/")
-    String home() {
-        return "This is the index page!";
+    public String home() {
+        return "Variables";
+        //return "Variables.html";
     }
 
     public static void main(String[] args) {
